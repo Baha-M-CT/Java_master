b8e10f96438f9814ab881b4bddfa400f09cb6a5c
GitForProjects <mametjob@gmail.com>
2022-11-02 21:27:12 -0400

    cleaning 2

--- src/main/java/New_Branch_Test.js
+++ src/main/java/New_Branch_Test.js
@@ -1,5 +1,1 @@
 console.log('First commit.');
-console.log('Second commit.');
-console.log('Third commit new');
-console.log('Third commit new. I have added new code here (Baha)');
-console.log('Forth commit.')
